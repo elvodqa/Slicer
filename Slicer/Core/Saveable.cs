bba3dc3ab5d9e10fb7eab7a162355f156c95657a
elvodqa <emircengiz21@gmail.com>
2023-06-28 17:28:18 +0300

    Init

---
+++ Slicer/Core/Saveable.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Slicer.Core
+{
+	public class Saveable : Attribute
+	{
+		public Saveable() { }
+
+        public static void SaveAs(string fileName)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly(); 
+            Type[] types = assembly.GetTypes();
+
+            var saveableProperties = types.SelectMany(t => t.GetProperties())
+                                          .Where(p => Attribute.IsDefined(p, typeof(Saveable)));
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                foreach (var property in saveableProperties)
+                {
+                    string line = $"{property.Name}: {property.GetValue(null)}";
+                    writer.WriteLine(line);
+                }
+            }
+        }
+
+        public static void LoadFrom(string fileName)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type[] types = assembly.GetTypes();
+
+            var saveableProperties = types.SelectMany(t => t.GetProperties())
+                                          .Where(p => Attribute.IsDefined(p, typeof(Saveable)));
+
+            string[] lines = File.ReadAllLines(fileName);
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(':');
+                if (parts.Length != 2)
+                    continue;
+
+                string propertyName = parts[0].Trim();
+                string propertyValue = parts[1].Trim();
+
+                var property = saveableProperties.FirstOrDefault(p => p.Name == propertyName);
+                if (property != null)
+                {
+                    object value = Convert.ChangeType(propertyValue, property.PropertyType);
+                    property.SetValue(null, value);
+                }
+            }
+        }
+    }
+}
+
---
+++ Slicer/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//using var game = new Slicer.Game1();
+//game.Run();
+
+using Slicer.Core;
+
+
+public class MyClass
+{
+    [Saveable]
+    public string Name { get; set; }
+
+    [Saveable]
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Age: {Age}";
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Saveable.SaveAs("test.txt");
+    }
+}
